26c7a2a556bb763161d010d7976e5b55dfccc1a9
Jesse Reichel <31119775+KryoKorpz@users.noreply.github.com>
2017-11-17 10:14:15 -0800

    init

---
+++ fullstack/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Test Text
+      </div>
+    );
+  }
+}
+
+export default App;
